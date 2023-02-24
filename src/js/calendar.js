388628dab942305d0bbef8124bab98c30299c825
natalynoele <natalynoele@gmail.com>
2023-02-24 21:08:27 +0200

    refactoring date handlers

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,9 +1,9 @@
-
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { refs } from './refs';
-let selectedDate = '';
+import { formatDate } from './functions/helper';
+
 if (
   window.location.pathname === '/' ||
   window.location.pathname === '/index.html'
@@ -19,28 +19,19 @@ if (
     onClose: function (selectedDates, dateStr, instance) {
       instance.input.nextElementSibling.classList.remove('open');
       instance.input.nextElementSibling.classList.add('close');
-      if (!selectedDates[0]) {
-        return;
-      }
-      instance.input.dataset.date = setDateForFetch(selectedDates[0]);
-      instance.input.dataset.sort = setDateForSort(selectedDates[0]);
+      isDateSelect(selectedDates[0]);
+      instance.input.setAttribute(
+        'data-date',
+        formatDate(selectedDates[0], 'd.m.y')
+      );
       instance.input.parentNode.classList.add('active');
-      selectedDate = selectedDates[0];
     },
     onOpen: function (selectedDates, dateStr, instance) {
       instance.input.nextElementSibling.classList.remove('close');
       instance.input.nextElementSibling.classList.add('open');
-      if (!selectedDates[0]) {
-        return;
-      }
-    },
-    onValueUpdate(selectedDates, date, instance) {
-      selectedDate = selectedDates[0];
     },
     onMonthChange(selectedDates, dateStr, instance) {
-      if (!selectedDates[0]) {
-        return;
-      }
+      isDateSelect(selectedDates[0]);
       selectedDates[0] = new Date(
         selectedDates[0].setMonth(instance.currentMonth)
       );
@@ -50,20 +41,15 @@ if (
       instance.selectedDateElem.dateObj = selectedDates[0];
       insertSelectedDate(selectedDates, dateStr, instance);
       setSelectedElementClass(selectedDates, instance);
-      selectedDate = selectedDates[0];
       return { selectedDates, dateStr };
     },
     onYearChange: function (selectedDates, dateStr, instance) {
-      if (!selectedDates[0]) {
-        return;
-      }
       isDateSelect(selectedDates[0]);
       selectedDates[0] = new Date(
         selectedDates[0].setFullYear(instance.currentYear)
       );
       insertSelectedDate(selectedDates, dateStr, instance);
       setSelectedElementClass(selectedDates, instance);
-      selectedDate = selectedDates[0];
       return { selectedDates, dateStr };
     },
   };
@@ -77,40 +63,15 @@ if (
     selectedEl.classList.add('selected');
   }
 
-  function setDateForFetch(date) {
-    const selectedDate = extractDateElements(date);
-    return `${selectedDate.year}${addLeadingZero(
-      selectedDate.month + 1
-    )}${addLeadingZero(selectedDate.day)}`;
-  }
-
-  function setDateForSort(date) {
-    const selectedDate = extractDateElements(date);
-    console.log(selectedDate.month);
-    return `${addLeadingZero(selectedDate.day)}.${addLeadingZero(
-      selectedDate.month + 1
-    )}.${selectedDate.year}`;
-  }
-
-  function extractDateElements(date) {
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-    return { year, month, day };
-  }
-
-  function setDateForAriaLabel(date) {
-    const year = date.getFullYear();
-    const month = date.toLocaleString('en', { month: 'long' });
-    const day = date.getDate();
-    return `${month} ${day}, ${year}`;
-  }
-
   function insertSelectedDate(selectedDates, dateStr, instance) {
     dateStr = selectedDates[0].toLocaleDateString('en-GB');
     instance.input.value = dateStr;
     instance.selectedDateElem.dateObj = selectedDates[0];
-    instance.selectedDateElem.ariaLabel = setDateForAriaLabel(selectedDates[0]);
+    instance.selectedDateElem.ariaLabel = formatDate(
+      selectedDates[0],
+      'm d, y',
+      'en'
+    );
     instance.latestSelectedDateObj = selectedDates[0];
   }
 
@@ -123,7 +84,6 @@ if (
 
   function isDateSelect(date) {
     if (!date) {
-      Notify.info('Please, select a Date');
       return;
     }
   }
